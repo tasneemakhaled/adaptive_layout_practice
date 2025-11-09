0bf57c6221c8946e60136223dadba486b4b896a3
tasneemkhaled <khaledtasneem402@gmail.com>
2025-11-09 09:49:33 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/adaptive_layout_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.adaptive_layout_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:adaptive_layout_practice/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+    // return MaterialApp(home: HomeView());
+  }
+}
